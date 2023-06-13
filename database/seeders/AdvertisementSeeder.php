b0630ec83038c24a98f6b8524fe138d3f23db524
silvialep <silvia.lepore_sl@libero.it>
2023-06-13 20:41:07 +0200

    feat: seeded advertisements

--- database/seeders/AdvertisementSeeder.php
+++ database/seeders/AdvertisementSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Advertisement;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
 
 class AdvertisementSeeder extends Seeder
 {
@@ -14,6 +16,36 @@ class AdvertisementSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $advertisements = [
+            [
+                'name' => 'Standard',
+                'description' => 'La sponsorizzazione standard, per avere il tuo profilo in evidenza per 24 ore',
+                'price' => 2.99,
+            ],
+            [
+                'name' => 'Gold',
+                'description' => 'La sponsorizzazione per chi vuole emergere e avere per 72 ore il profilo in evidenza',
+                'price' => 5.99,
+            ],
+            [
+                'name' => 'Premium',
+                'description' => 'La sponsorizzazione premium, per ottenere il massimo della visibilità e avere il tuo profilo in evidenza per 144 ore',
+                'price' => 9.99,
+            ]
+              
+        ];
+
+        foreach ($advertisements as $advertisement) {
+
+            $newAdvertisement = new Advertisement();
+
+            $newAdvertisement->name = $advertisement['name'];
+            $newAdvertisement->slug = Str::slug($newAdvertisement->name, '-');
+            $newAdvertisement->description = $advertisement['description'];
+            $newAdvertisement->price = $advertisement['price'];
+            
+
+            $newAdvertisement->save();
+        }
     }
 }
