0f14e5cb7e436477ee834ba889a3ad678526069f
marcodedemo <marco.dedemo@gmail.com>
2023-06-20 15:19:47 +0200

    style: error page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,15 @@
 @extends('layouts.admin')
 
 @section('content')
-    <h2>NON HAI ACCESSO A QUESTO PROFILO</h2>
+
+<div id="notfound-container">
+    <img id="notfound-robot" src="{{Vite::asset('resources/img/notfound.svg')}}" alt="">
+</div>
+
+<div class="container alert alert-danger text-center">
+
+    <h2>Pagina non trovata</h2>
+    
+</div>
+
 @endsection
