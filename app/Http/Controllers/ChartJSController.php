a1cdf0de02d9789f95cafedc92c67921472f03fd
alessandroamormino <amormino.alessandro97@gmail.com>
2023-06-22 17:45:24 +0200

    feat: add second graph statistic reviews

--- app/Http/Controllers/ChartJSController.php
+++ app/Http/Controllers/ChartJSController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Message;
+use App\Models\Review;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB as DB;
@@ -16,7 +17,9 @@ class ChartJSController extends Controller
         $user = Auth::user();
         // memorizzo il profilo riferito allo user loggato
         $devLogged = $user->developer->id;
-        // MONTHNAME(created_at)
+        
+        // Messaggi
+
         $messages = Message::select(DB::raw("COUNT(*) as count"), DB::raw("DATE_FORMAT(created_at, '%m/%Y') as month_name"))
         ->where('developer_id', $devLogged)
         ->whereYear('created_at', date('Y'))
@@ -26,7 +29,22 @@ class ChartJSController extends Controller
         
         $labels = $messages->keys();
         $data = $messages->values();
+
+  
+        // Recensioni
+
+        $reviews = Review::select(DB::raw("COUNT(*) as count"), DB::raw("DATE_FORMAT(created_at, '%m/%Y') as month_name"))
+        ->where('developer_id', $devLogged)
+        ->whereYear('created_at', date('Y'))
+        ->groupBy(DB::raw("month_name"))
+        // ->orderBy('id','ASC')
+        ->pluck('count', 'month_name');
+        
+        $labelsReviews = $reviews->keys();
+        $dataReviews = $reviews->values();
+
               
-        return view('admin.statistics.chart', compact('labels', 'data'));
+        return view('admin.statistics.chart', compact('labels', 'data', 'labelsReviews', 'dataReviews'));
+
     }
 }
--- resources/views/Admin/Statistics/chart.blade.php
+++ resources/views/Admin/Statistics/chart.blade.php
@@ -3,9 +3,13 @@
 @section('content')
 
 <div class="container mt-3">
-  <h1>Statistiche</h1>
+  <h1 class="pb-3">Statistiche</h1>
 
-  <canvas id="myChart" height="100px"></canvas>
+  <h3>Messaggi Ricevuti</h3>
+  <canvas id="messagesChart" height="100px"></canvas>
+  <h3>Recensioni Ricevute</h3>
+  <canvas id="reviewsChart" height="100px"></canvas>
+
 </div>
 
 
@@ -14,6 +18,8 @@
 
 <script type="text/javascript">
   
+  // Messaggi
+
   const labels =  {{ Js::from($labels) }};
   const messages =  {{ Js::from($data) }};
 
@@ -41,10 +47,47 @@
       },
   };
 
-  const myChart = new Chart(
-      document.getElementById('myChart'),
+  const messagesChart = new Chart(
+      document.getElementById('messagesChart'),
       config
   );
+
+
+
+  // Recensioni
+
+  const labelsReviews =  {{ Js::from($labelsReviews) }};
+  const messagesReviews =  {{ Js::from($dataReviews) }};
+
+  const dataReviews = {
+      labels: labelsReviews,
+      datasets: [{
+          label: 'Recensioni per Mese/Anno',  
+          backgroundColor: '#D8FA96',
+          hoverBackgroundColor: '#B4F437',
+          data: messagesReviews,
+      }]
+  };
+
+  const configReviews = {
+      type: 'bar',
+      data: dataReviews,
+      // options: {}
+      options: {
+        scales: {
+            y: {
+                suggestedMin: 1,
+                suggestedMax: 10  
+            }
+        }
+      },
+  };
+
+  const reviewsChart = new Chart(
+      document.getElementById('reviewsChart'),
+      configReviews
+  );
+
 
 </script>
 
